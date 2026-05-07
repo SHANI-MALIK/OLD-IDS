3b017ffb9a7f26eeab008f926bbee88a6f10d01d
SHANI-MALIK <safdarkami723327@gmail.com>
2026-05-07 22:18:15 +0500

    Update shani.py

--- shani.py
+++ shani.py
@@ -234,20 +234,19 @@ def ____banner____():
     print(PINK + "╚══════════════════════════════════╝" + RESET)
 
     # INFO PANEL
-    print(GREEN + "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" + RESET)
-    print(f"{CYAN}[{GREEN}✓{CYAN}] \033[1;97mDeveloper  : \033[1;96mSHANI{RESET}")
-    print(f"{CYAN}[{GREEN}✓{CYAN}] \033[1;97mOwner      : \033[1;92mSHANI{RESET}")
-    print(f"{CYAN}[{GREEN}✓{CYAN}] \033[1;97mStatus     : \033[1;93mPREMIUM{RESET}")
-    print(f"{CYAN}[{GREEN}✓{CYAN}] \033[1;97mVersion    : \033[1;95m0.1{RESET}")
+print(GREEN + "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" + RESET)
 
-    print(f"{CYAN}[{GREEN}✓{CYAN}] \033[1;97mKey        : \033[1;96m{user_key if user_key else 'LOADING...'}{RESET}")
+print(f"{CYAN}[{GREEN}✓{CYAN}] \033[1;97mDeveloper  : \033[1;96mSHANI{RESET}")
+print(f"{CYAN}[{GREEN}✓{CYAN}] \033[1;97mOwner      : \033[1;92mSHANI{RESET}")
+print(f"{CYAN}[{GREEN}✓{CYAN}] \033[1;97mStatus     : \033[1;93mPREMIUM{RESET}")
+print(f"{CYAN}[{GREEN}✓{CYAN}] \033[1;97mVersion    : \033[1;95m0.1{RESET}")
 
-if exp:
-    print(f"{CYAN}[{GREEN}✓{CYAN}] Expiry     : {exp if exp else 'N/A'}{RESET}")
+print(f"{CYAN}[{GREEN}✓{CYAN}] \033[1;97mKey        : \033[1;96m{user_key if user_key else 'LOADING...'}{RESET}")
 
-if left:
-    print(f"{CYAN}[{GREEN}✓{CYAN}] Time Left  : {left if left else 'N/A'}{RESET}")
-    print(GREEN + "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" + RESET)
+print(f"{CYAN}[{GREEN}✓{CYAN}] \033[1;97mExpiry     : \033[1;96m{exp if exp else 'N/A'}{RESET}")
+print(f"{CYAN}[{GREEN}✓{CYAN}] \033[1;97mTime Left  : \033[1;96m{left if left else 'N/A'}{RESET}")
+
+print(GREEN + "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" + RESET)
 if __name__ == "__main__":
     ____banner____()
     
