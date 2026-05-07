1d0d5365f22ff65f86f326a28ba1613f0bf77781
SHANI-MALIK <safdarkami723327@gmail.com>
2026-05-07 23:40:48 +0500

    Update shani.py

--- shani.py
+++ shani.py
@@ -224,8 +224,8 @@ def ____banner____():
     "╚════██║██╔══██║██╔══██║██║╚██╗██║██║",
     "███████║██║  ██║██║  ██║██║ ╚████║██║",
     "╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝╚═╝"
-    ] print(RED + "I'm a gangster without a Gang" + RESET)
-
+    ]
+    print(RED + "I'm a gangster without a Gang" + RESET)
     for line in logo:
         print(CYAN + "║" + line.center(width) + "║")
 
